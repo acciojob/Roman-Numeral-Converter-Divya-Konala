5b515a5f34b4dfca22bcec0a3643a7e0ad7cd55a
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-01-23 17:52:59 +0530

    to roman

--- script.js
+++ script.js
@@ -10,11 +10,22 @@ function convertToRoman(num) {
     };
 
   //your code here
+    let thousand=Math.floor(num/1000);
+    let hundred=Math.floor((num%1000)/100);
+    let ten=Math.floor((num%100)/10);
+    let one=Math.floor((num%10));
+
+    
+  let ones=['','I','II','III','IV','V','VI','VII','VIII','IX'];
+  let tens=['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC'];
+  let hundreds=['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM'];
+  let thousands=['','M','MM','MMM'];
+  return (thousands[thousand]+hundreds[hundred]+tens[ten]+ones[one]);
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+// console.log(convertToRoman(3597));
 
 
 
